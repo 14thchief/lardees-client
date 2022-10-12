6cd060cbcf4643efd86aa2b161115341a6198089
chief omah <14thchief@gmail.com>
2022-10-12 01:05:11 +0100

    remove buggy dragula

--- src/features/hero/Hero.jsx
+++ src/features/hero/Hero.jsx
@@ -6,7 +6,6 @@ import MidCard from "../../components/Cards/MidCard";
 import SmallCard from "../../components/Cards/SmallCard";
 import Header from "../../components/Header/Header";
 import classes from './Hero.module.scss';
-import dragula from "dragula";
 
 const Hero = () => {
     const leftBox = useRef(null);
@@ -14,30 +13,7 @@ const Hero = () => {
     
     useEffect(()=>{
         console.log(leftBox.current, rightBox.current)
-        dragula([leftBox.current, rightBox.current], {
-            isContainer: function (el) {
-              return false; // only elements in drake.containers will be taken into account
-            },
-            moves: function (el, source, handle, sibling) {
-              return true; // elements are always draggable by default
-            },
-            accepts: function (el, target, source, sibling) {
-              return true; // elements can be dropped in any of the `containers` by default
-            },
-            invalid: function (el, handle) {
-              return false; // don't prevent any drags from initiating by default
-            },
-            direction: 'horizontal',             // X axis is considered when determining where an element would be dropped
-            copy: false,                       // elements are moved by default, not copied
-            copySortSource: false,             // elements in copy-source containers can be reordered
-            revertOnSpill: false,              // spilling will put the element back where it was dragged from, if this is true
-            removeOnSpill: false,              // spilling will `.remove` the element, if this is true
-            mirrorContainer: document.body,    // set the element that gets mirror elements appended
-            ignoreInputTextSelection: true,     // allows users to select input text, see details below
-            slideFactorX: 0,               // allows users to select the amount of movement on the X axis before it is considered a drag instead of a click
-            slideFactorY: 0,               // allows users to select the amount of movement on the Y axis before it is considered a drag instead of a click
-          });
-    })
+    },[])
 
     return (
         <>
